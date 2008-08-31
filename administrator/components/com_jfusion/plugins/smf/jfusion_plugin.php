dd4d63bb2d57e226ab08875c6747b33ba981531e
mariusvr <mariusvr@dd2d3cf9-e250-0410-90e7-b9081df83568>
2008-08-31 23:21:50 +0000

    added buffer filtering function for SMF

--- administrator/components/com_jfusion/plugins/smf/jfusion_plugin.php
+++ administrator/components/com_jfusion/plugins/smf/jfusion_plugin.php
@@ -273,5 +273,67 @@ class JFusionPlugin_smf extends JFusionPlugin{
 	}
 
 
+
+	function parseBody(&$buffer, $baseURL, $fullURL, $integratedURL)
+	{
+		static $regex_body, $replace_body;
+
+		if ( ! $regex_body || ! $replace_body )
+		{
+			// Define our preg arrayshttp://www.jfusion.org/administrator/index.php?option=com_extplorer#
+			$regex_body		= array();
+			$replace_body	= array();
+
+			//convert relative links with query into absolute links
+			$regex_body[]	= '#href="./(.*)\?(.*)"#mS';
+			$replace_body[]	= 'href="'.$baseURL.'&jfile=$1&$2"';
+
+			//convert relative links without query into absolute links
+			$regex_body[]	= '#href="./(.*)"#mS';
+			$replace_body[]	= 'href="'.$baseURL.'&jfile=$1"';
+
+			//convert relative links from images into absolute links
+			$regex_body[]	= '#(src="|url\()./(.*)("|\))#mS';
+			$replace_body[]	= '$1'.$integratedURL.'$2$3"';
+
+			//convert links to the same page with anchors
+			$regex_body[]	= '#href="\#(.*?)"#';
+			$replace_body[]	= 'href="'.$fullURL.'&#$1"';
+
+			//update site URLs to the new Joomla URLS
+			$regex_body[]	= "#$integratedURL(.*)\?(.*)\"#mS";
+			$replace_body[]	= $baseURL . '&jfile=$1&$2"';
+
+			//convert action URLs inside forms to absolute URLs
+			//$regex_body[]	= '#action="(.*)"#mS';
+			//$replace_body[]	= 'action="'.$integratedURL.'/"';
+
+		}
+
+		$buffer = preg_replace($regex_body, $replace_body, $buffer);
+	}
+
+	function parseHeader(&$buffer, $baseURL, $fullURL, $integratedURL)
+	{
+		static $regex_header, $replace_header;
+
+		if ( ! $regex_header || ! $replace_header )
+		{
+			// Define our preg arrays
+			$regex_header		= array();
+			$replace_header	= array();
+
+			//convert relative links into absolute links
+			$regex_header[]	= '#(href|src)=("./|"/)(.*?)"#mS';
+			$replace_header[]	= 'href="'.$integratedURL.'$3"';
+
+			//$regex_header[]	= '#(href|src)="(.*)"#mS';
+			//$replace_header[]	= 'href="'.$integratedURL.'$2"';
+
+		}
+
+		$buffer = preg_replace($regex_header, $replace_header, $buffer);
+}
+
 }
 
