b5b5108f1a34b5cc6dc139a876c6d49389e2a924
fannoj@gmail.com <fannoj@gmail.com@dd2d3cf9-e250-0410-90e7-b9081df83568>
2009-03-06 10:48:40 +0000

    cleaned up JS

--- administrator/components/com_jfusion/plugins/smf/js/script.js
+++ administrator/components/com_jfusion/plugins/smf/js/script.js
@@ -9,7 +9,7 @@ function jfusion_doQuote(messageid, cur_session_id)
 		{
 			if (typeof window.ajax_indicator == "function")
 				ajax_indicator(true);
-			jfusion_getXMLDocument(jf_scripturl + "&action=quotefast;quote=" + messageid + ";sesc=" + cur_session_id + ";xml", jfusion_onDocReceived);
+			getXMLDocument(jf_scripturl + "&action=quotefast;quote=" + messageid + ";sesc=" + cur_session_id + ";xml", onDocReceived);
 		}
 		else
 			reqWin(jf_scripturl + "&action=quotefast;quote=" + messageid + ";sesc=" + cur_session_id, 240, 90);
@@ -19,50 +19,4 @@ function jfusion_doQuote(messageid, cur_session_id)
 		else
 			window.location.hash = "#quickreply";
 	}
-}
-function jfusion_onDocReceived(XMLDoc)
-{
-	alert("jfusion_onDocReceived:"+text);
-	for (var i = 0; i < XMLDoc.getElementsByTagName("quote")[0].childNodes.length; i++){
-		text += XMLDoc.getElementsByTagName("quote")[0].childNodes[i].nodeValue;
-	}
-
-	replaceText(text, document.forms.postmodify.message);
-
-	if (typeof window.ajax_indicator == "function")
-		ajax_indicator(false);
-}
-// Load an XML document using XMLHttpRequest.
-function jfusion_getXMLDocument(url, callback)
-{
-	if (!window.XMLHttpRequest)
-		return false;
-	var callit = callback;
-	var myDoc = new XMLHttpRequest();
-	if (typeof(callback) != "undefined")
-	{
-
-		myDoc.onreadystatechange = function ()
-		{
-			if (myDoc.readyState != 4)
-				return;
-				//alert(myDoc.responseText);
-			if ( myDoc.responseXML != null && myDoc.status == 200 ) {
-				//callback(myDoc.responseXML);
-				var text = "";
-				for (var i = 0; i < myDoc.responseXML.getElementsByTagName("quote")[0].childNodes.length; i++){
-					text += myDoc.responseXML.getElementsByTagName("quote")[0].childNodes[i].nodeValue;
-				}
-
-				replaceText(text, document.forms.postmodify.message);
-
-				if (typeof window.ajax_indicator == "function")
-					ajax_indicator(false);
-			}
-		};
-	}
-	myDoc.open("GET", url, true);
-	myDoc.send(null);
-
-	return true;
 }
